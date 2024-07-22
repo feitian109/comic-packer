cca2210a9a9f5434e2d49622c37956c07ce68d3b
feitian109 <1093921366@qq.com>
2024-07-22 15:48:10 +0800

    Use pathlib replace os

--- comic_packer.py
+++ comic_packer.py
@@ -1,8 +1,8 @@
-import os
 import re
 import sys
 import shutil
 import zipfile
+from pathlib import Path
 import xml.etree.ElementTree as ET
 
 # 生成封面
@@ -40,7 +40,7 @@ url = {
 
 
 # 根据文件夹名推测信息
-def analysis(comic_name):
+def analysis(comic_name) -> dict:
     # 分析 Comic info，拆分成 `parts`
     raw = re.split(r"(\[[^]]+\]|\([^)]+\)|【[^】]+】)", comic_name)
     parts = [item.strip() for item in raw if item not in ["", " "]]
@@ -64,32 +64,29 @@ def analysis(comic_name):
     return info
 
 
-def zip(folder_path, output_path):
-    zip_path = os.path.join(output_path, zip_name)
+def zip(comic_path: Path, output_path: Path):
+    zip_path = output_path / zip_name
     with zipfile.ZipFile(zip_path, "a") as z:
-        pic_names = []
-        for file_name in os.listdir(folder_path):
+        pic_paths = []
+        for file_path in comic_path.iterdir():
             # 通过扩展名判断文件是否是图片
-            if re.search(r"\.(jpg|jpeg|png)$", file_name):
-                pic_names.append(file_name)
+            if re.search(r"\.(jpg|jpeg|png)$", file_path.name):
+                pic_paths.append(file_path)
 
         # 生成封面
         if generate_cover:
-            pic_names.sort()
-            shutil.copy(
-                os.path.join(folder_path, pic_names[0]),
-                os.path.join(output_path, "cover" + os.path.splitext(pic_names[0])[-1]),
-            )
+            pic_paths.sort(key=lambda path: path.name)
+            cover_path = pic_paths[0]
+            shutil.copy(cover_path, output_path / ("cover" + cover_path.suffix))
 
         # 写入压缩包
-        for pic_name in pic_names:
-            pic_path = os.path.join(folder_path, pic_name)
-            z.write(pic_path, pic_name)
+        for pic_path in pic_paths:
+            z.write(pic_path, pic_path.name)
 
 
-def generate_info(comic_name, output_path):
-    info = analysis(comic_name)
-    info_path = os.path.join(output_path, "ComicInfo.xml")
+def generate_info(comic_path: Path, output_path: Path):
+    info = analysis(comic_path.name)
+    info_path = output_path / "ComicInfo.xml"
 
     # 生成 `root`
     root = ET.Element(
@@ -118,39 +115,46 @@ def generate_info(comic_name, output_path):
 
 
 # 打包
-def pack(comic_name, folder_path, output_path):
-    print(f"Packing: {folder_path}...", end="")
-    zip(folder_path, output_path)
-    generate_info(comic_name, output_path)
+def pack(comic_path: Path, output_path: Path):
+    print(f"Packing: {comic_path}...", end="")
+    zip(comic_path, output_path)
+    generate_info(comic_path, output_path)
     print("Done")
 
 
-if __name__ == "__main__":
+def main():
     sys.argv.pop(0)
     for arg in sys.argv:
+        work_path = Path(arg)
+
         # 不存在该路径
-        if not os.path.exists(arg):
-            print(f"Directory '{arg}' not exist")
+        if not work_path.exists():
+            print(f"Directory '{work_path}' not exist")
 
         # 路径为文件夹时
-        elif os.path.isdir(arg):
-            for comic_name in os.listdir(arg):  # 遍历文件夹名
+        elif work_path.is_dir():
+            for comic_path in work_path.iterdir():  # 遍历漫画路径
                 # 跳过输出文件夹
-                if comic_name == "output":
+                if comic_path.name == "output":
                     continue
 
-                folder_path = os.path.join(arg, comic_name)  # 存储漫画的文件夹
+                if comic_path.is_dir():
+                    output_path = work_path / "output" / comic_path.name  # 输出路径
 
-                if os.path.isdir(folder_path):
-                    output_path = os.path.join(arg, "output", comic_name)  # 输出路径
-
-                    # 如果已存在文件夹
-                    if os.path.exists(output_path):
+                    try:
+                        output_path.mkdir(parents=True, exist_ok=False)
+                        
+                    # 输出路径已存在时
+                    except:
                         print(f"Warning: {output_path} exist, skip...")
-                    else:
-                        os.makedirs(output_path)
-                        pack(comic_name, folder_path, output_path)
+                        continue
 
-        # 路径为文件时
+                    pack(comic_path, output_path)
+
+        # 路径为文件或其他情况时
         else:
             print("Error: Not a folder")
+
+
+if __name__ == "__main__":
+    main()
